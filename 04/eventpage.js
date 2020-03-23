660ec04c0b7ace693f4f8f7928e15bb5c09bf0cd
kuro <kisuvitinh@gmail.com>
2020-03-23 17:18:09 +0700

    4-search with wiki

---
+++ 04/eventpage.js
@@ -0,0 +1,27 @@
+let contextMenuItem = {
+    "id":"search",
+    "title":"Search",
+    "contexts":["selection"]
+};
+
+chrome.contextMenus.create(contextMenuItem);
+
+function fixedEncodeURI(str){
+    return encodeURI(str).replace(/%5B/g,'[').replace(/%5D/g,']');
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if(clickData.menuItemId == "search" && clickData.selectionText){
+        let urlWiki = "https://vi.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText)
+        let createData = {
+            "url":urlWiki,
+            "type":"popup",
+            "top":5,
+            "left":5,
+            "width": screen.availWidth/2,
+            "height":screen.availHeight/2
+        }
+        chrome.windows.create(createData, function(){})
+    }
+
+})
